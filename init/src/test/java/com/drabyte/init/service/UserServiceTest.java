48ca267db9a5d1bd64b832b0f5700ce1147bbb8f
drajskip <pdrajski@gmail.com>
2013-04-07 17:01:49 +0200

    Test Fix

--- init/src/test/java/com/drabyte/init/service/UserServiceTest.java
+++ init/src/test/java/com/drabyte/init/service/UserServiceTest.java
@@ -1,10 +1,9 @@
 package com.drabyte.init.service;
 
-import static org.fest.assertions.Assertions.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -15,9 +14,9 @@ public class UserServiceTest {
 	@Autowired
 	private UserService userService;
 
-	@Test
+	@Test(expected= UsernameNotFoundException.class)
 	public void shouldReturnLogin() {
-	 assertThat(userService.loadUserByUsername("bleble")).isNull();
+	 userService.loadUserByUsername("bleble");
 	}
 
 
